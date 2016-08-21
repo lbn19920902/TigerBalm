22e2239b1ee771bad5ac4580a9671c61923fff7d
ryancheunggit <ryanzjlib@gmail.com>
2016-08-21 08:43:56 -0400

    add more documentation, known issue

--- data_feeding_helper.py
+++ data_feeding_helper.py
@@ -2,19 +2,25 @@
     @ Ren Zhang : ryanzjlib@gmail.com 
     last update : 2016-08-17
 '''
+from __future__ import print_function
+from __future__ import division
 
-def continuous_chunks(indices, num_chunks):
+def continuous_chunks(lst, num_chunks):
 	"""
+		generate contiunes smaller chunks from a given list
 		
+		Examples:
+		-------------------------------------
+		>>> a = continuous_chunks(range(11), 3)
+		>>> for i in range(3):
+				print(a.next())		
+		[0, 1, 2, 3]
+		[4, 5, 6, 7]
+		[8, 9, 10]
 	"""
-	num_the_current_chunk = int(1.0 * len(indices) / num_chunks + 0.5)
-
+	num_the_current_chunk = int(len(lst) / num_chunks + 0.5)
 	for i in xrange(0, num_chunks - 1):
-		yield indices[i*num_the_current_chunk:(i+1)*num_the_current_chunk]
-	yield indices[(num_chunks -1)* num_the_current_chunk:]
+		yield lst[i*num_the_current_chunk:(i+1)*num_the_current_chunk]
+	yield lst[(num_chunks -1)* num_the_current_chunk:]
 
 
-a = continuous_chunks(range(11), 3)
-print(a.next())
-print(a.next())
-print(a.next())
--- sklearn_helper.py
+++ sklearn_helper.py
@@ -32,6 +32,7 @@ def plot_grid_search_result(grid_scores, x, y = "mean_validation_score", hue = N
 def plot_roc_curve(roc_curves, save_to_file = False, file_name = "plot.png"):
 	"""expecting roc_curves to be a list of tuples, ("method_name", (fpr, tpr, threshold)))"""
 	num_methods = len(roc_curves)
+	# TODO: the number of colors still not working
 	sns.palplot(sns.color_palette("hls", num_methods))
 	plt.figure()
 	plt.xlim([-0.05,1.05])
