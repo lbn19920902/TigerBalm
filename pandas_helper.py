d8300e2b30ac3c36cd49d5ec82729a3c25ce9caa
ryancheunggit <ryanzjlib@gmail.com>
2016-09-23 14:42:55 -0400

    update large csv file reader

--- pandas_helper.py
+++ pandas_helper.py
@@ -4,13 +4,16 @@
 '''
 from __future__ import print_function
 
-def pandas_read_large_csv(file, chunksize = 10 ** 4):
+def pandas_read_large_csv(file, chunksize = 10 ** 5, **kwargs):
     '''
         a wrapper function for reading huge csv files by smaller chuncks several times
+        updated Sept-22-16 can specify dtype, also increased chunksize by 10 
+        example:
+        data = pandas_read_large_csv("data/train_numeric.csv", chunksize = 100000, dtype = np.float32)
     '''
     import pandas as pd
     chunks = []
-    for chunk in pd.read_csv(file, chunksize = chunksize):
+    for chunk in pd.read_csv(file, chunksize = chunksize, **kwargs):
         chunks.append(chunk)
     return pd.concat(chunks)
 
